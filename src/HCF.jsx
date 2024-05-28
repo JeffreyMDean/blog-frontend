72418032f894ea0c435c0617bab220c952eac9c2
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-05-28 14:52:09 -0400

    Added footer/header,postsindex/postsnew/content functions and moved each into seperate files

---
+++ src/HCF.jsx
@@ -0,0 +1,12 @@
+import { Content } from './Content';
+import { Header, Footer } from './App';
+
+function HCF() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
